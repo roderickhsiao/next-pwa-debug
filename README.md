29b4d297eecf96b682926f437753db8e495409b7
Roderick Hsiao <roderickhsiao@gmail.com>
2024-01-11 10:54:55 -0800

    Initial commit from Create Next App

---
+++ README.md
@@ -0,0 +1,19 @@
+# next-pwa - reproduction-template
+
+This example is a reproduction template.
+
+## Usage
+
+Execute [`create-next-app`](https://github.com/vercel/next.js/tree/canary/packages/create-next-app) with [npm](https://docs.npmjs.com/cli/init), [Yarn](https://yarnpkg.com/lang/en/docs/cli/create/), or [pnpm](https://pnpm.io) to bootstrap the example:
+
+```bash
+npx create-next-app --example https://github.com/DuCanhGH/next-pwa/tree/master/examples/reproduction-template reproduction
+```
+
+```bash
+yarn create next-app --example https://github.com/DuCanhGH/next-pwa/tree/master/examples/reproduction-template reproduction
+```
+
+```bash
+pnpm create next-app --example https://github.com/DuCanhGH/next-pwa/tree/master/examples/reproduction-template reproduction
+```
---
+++ app/layout.tsx
@@ -0,0 +1,40 @@
+import type { Metadata, Viewport } from "next";
+import type { ReactNode } from "react";
+
+const APP_NAME = "next-pwa example";
+const APP_DESCRIPTION = "This is an example of using next-pwa";
+
+export const metadata: Metadata = {
+  applicationName: APP_NAME,
+  title: {
+    default: APP_NAME,
+    template: "%s - PWA App",
+  },
+  description: APP_DESCRIPTION,
+  manifest: "/manifest.json",
+  appleWebApp: {
+    capable: true,
+    statusBarStyle: "default",
+    title: APP_NAME,
+  },
+  formatDetection: {
+    telephone: false,
+  },
+  icons: {
+    shortcut: "/favicon.ico",
+    apple: [{ url: "/icons/apple-touch-icon.png", sizes: "180x180" }],
+  },
+};
+
+export const viewport: Viewport = {
+  themeColor: "#FFFFFF",
+};
+
+export default function RootLayout({ children }: { children: ReactNode }) {
+  return (
+    <html lang="en-US">
+      <head />
+      <body>{children}</body>
+    </html>
+  );
+}
